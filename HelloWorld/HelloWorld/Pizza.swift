32e8064a0ae804a4fc9546444c31e58c55e050d5
takushi <takushi@mfmf.me>
2015-09-06 11:22:46 +0900

    commit

---
+++ HelloWorld/HelloWorld/Pizza.swift
@@ -0,0 +1,17 @@
+//
+//  Pizza.swift
+//  HelloWorld
+//
+//  Created by Homma Takushi on 2015/09/05.
+//  Copyright (c) 2015年 mfmf.me. All rights reserved.
+//
+
+import Foundation
+
+class Pizza {
+  private(set) var name: String
+  
+  init() {
+    self.name = "ピザ"
+  }
+}
---
+++ HelloWorld/HelloWorld/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  HelloWorld
+//
+//  Created by Homma Takushi on 2015/09/05.
+//  Copyright (c) 2015年 mfmf.me. All rights reserved.
+//
+
+import Foundation
+
+var pizza: Pizza = Pizza()
+//pizza.name = "ピッツア" //Cannot assign to 'name' in 'pizza'
+println(pizza.name)
